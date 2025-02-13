9ed24fdd5ef36159f8b77cf150a3f34917e52f6f
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2025-02-13 17:10:07 +0600

    svg image change

--- src/Components/BookMark.jsx
+++ src/Components/BookMark.jsx
@@ -9,7 +9,7 @@ const BookMark = ({addBlog,markRead}) => {
     
     return (
         <div className="w-1/3 bg-gray-200 px-2">
-            <span className="flex items-center text-xl font-bold text-blue-600"> <PiTimerFill className="text-red-600 text-2xl" /> Reading Time: {markRead}</span>
+            <span className="flex items-center text-xl font-bold text-blue-600 "> <PiTimerFill className="text-red-600 text-2xl " /> Reading Time: {markRead}</span>
             <h1 className="text-2xl font-bold">Bookmarked Blogs: {addBlog.length}</h1>
             <div>
                 {
